d1ad357cd137c3f9895d313513d1105de31cb488
Andy Stevens <57344820+apstevens@users.noreply.github.com>
2021-02-01 21:13:12 +0000

    Updated README and added comments to script.js

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# number-guesser
+<h1>Number Guessing Game</h1>
  
+<p>The scope of this project, was to create a small number guessing using functions to power the game.</p>
+
+<h4>Languages</h4>
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+</ul>
--- script.js
+++ script.js
@@ -2,17 +2,27 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+/**
+ * Function called at the start of each round
+ * returns a random integer between 0 and 9
+ */
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 }
-
+/**
+ * Function called each round to determine which guess is closest to 
+ * the target number
+ * return true if humanGuess wins
+ */
 const compareGuesses = (humanGuess, computerGuess, target) => {
     const humanDifference = Math.abs(target - humanGuess);
     const computerDifference = Math.abs(target - computerGuess);
     return humanDifference <= computerDifference;
 }
 
+/**
+ * Function to correctly increase winners score after each round
+ */
 const updateScore = winner => {
     if (winner === 'human') {
         humanScore++;
@@ -21,4 +31,7 @@ const updateScore = winner => {
     }
 }
 
+/**
+ * Function to update the round number after each round
+ */
 const advanceRound = () => currentRoundNumber++;
